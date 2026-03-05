aeebf177bc89b85fb7ab982a9b65aa250dcc7c46
Gala <galchonok_gala@mail.ru>
2026-03-05 18:41:27 +0300

    Работа над hw-2 в процессе

--- hw-2.js
+++ hw-2.js
@@ -1,81 +1,71 @@
 let a;
 a = 10;
-alert('Задание1 (первое значение): ' + a);a
- = 20;a
-lert('Задание1 (второе значение): ' + a);  
-
+alert('Задание 1 (первое значение): ' + a);
+a = 20;
+alert('Задание 1 (второе значение): ' + a);
 
 let firstIPhoneYear = 2007;
 alert('Задание 2: Год выпуска первого iPhone - ' + firstIPhoneYear);
 
+let jsCreator = 'Брендан Айк';
+alert('Задание 3: Создатель JavaScript - ' + jsCreator);
 
-  let jsCreator = 'Брендан Айк
-';alert('Задание 3: Создатель JavaScript - ' + jsCreator
+let num1 = 10;
+let num2 = 2;
+alert('Задание 4: Сумма 10 и 2 = ' + (num1 + num2));
+alert('Задание 4: Разность 10 и 2 = ' + (num1 - num2));
+alert('Задание 4: Произведение 10 и 2 = ' + (num1 * num2));
+alert('Задание 4: Частное 10 и 2 = ' + (num1 / num2));
 
+let result = 2 ** 5;
+alert('Задание 5: 2 в 5-й степени = ' + result);
 
-);  let num1 =
- 10;let num2 
-= 2;alert('Задание4: Сумма 10 и 2 = ' + (num1 + num2
-));alert('Задание4: Разность 10 и 2 = ' + (num1 - num2)
-);alert('Задание4: Произведение 10 и 2 = ' + (num1 * num2))
-;alert('Задание4: Частное 10 и 2 = ' + (num1 / num2));
+let a6 = 9;
+let b6 = 2;
+let remainder = a6 % b6;
+alert('Задание 6: Остаток от деления 9 на 2 = ' + remainder);
 
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert('Задание 7: Результат = ' + num);
 
-  let result = 2 ** 
-5;alert('Задание5: 2 в 5-й степени = ' +result);
+let age = prompt('Задание 8: Сколько вам лет?');
+alert('Задание 8: Вам ' + age + ' лет.');
 
+let user = {
+    name: 'Иван',
+    age: 25,
+    isAdmin: false
+};
+alert('Задание 9: Имя: ' + user.name + ', Возраст: ' + user.age + ', Админ: ' + user.isAdmin);
 
-  let a6 = 
-9;let b6 = 
-2;let remainder =a6 % b6
-;alert('Задание6: Остаток от деления 9 на 2 = ' +remainder); 
+let userName = prompt('Задание 10: Как вас зовут?');
+alert(`Задание 10: Привет, ${userName}!`);
 
+alert('А теперь - математический фокус!');
+let number = prompt('Загадайте любое число:');
+number = Number(number);
 
- let num = 1
-;num += 5
-;num -= 3
-;num *= 7
-;num /= 3
-;num++
-;num--
-;alert('Задание 7: Результат = ' + num)
-
-
-
-;   let age = prompt('Задание8: Сколько вам лет?
-');alert('Задание8: Вам ' + age + ' лет.'
-
-
-);  let user
- = {    name: 'Ив
-ан',    age:
- 25,    isAdmin: f
-al
-se};alert('Задание 9: Имя: ' + user.name+ ',Возраст: ' + user.age+ ',Админ: ' + user.isAdmin);
-
-
-
-   let userName = prompt('Задание10: Как вас зовут?'
-);alert(`Задание10: Привет, ${userName}!`)
-
-
-;  alert('Атеперь - математический фокус!'
-);let number = prompt('Загадайте любое число:'
-);number = Number(number
-
-); if (isNaN(number)
-) {    alert('Ошибка: нужно ввести число!
-');} els
-e {    letdoubled = number * 
-2;    alert('Удвоенное число: ' +doubled)
-;   
-     letplusTen = doubled + 10;
+if (isNaN(number)) {
+    alert('Ошибка: нужно ввести число!');
+} else {
+    let doubled = number * 2;
+    alert('Удвоенное число: ' + doubled);
+    
+    let plusTen = doubled + 10;
     alert('Прибавили 10: ' + plusTen);
     
-    lethalved = plusTen / 2; 
-   alert('Разделили на 2: ' +halved);  
+    let halved = plusTen / 2;
+    alert('Разделили на 2: ' + halved);
     
-  letfinalResult = halved -number;    
-alert('Вычли исходное число: ' +finalResult);     
-   a
-lert('Получилось5! Задание выполнено!');}
+    let finalResult = halved - number;
+    alert('Вычли исходное число: ' + finalResult);
+    
+    alert('Получилось 5! Задание выполнено!');
+}
+
